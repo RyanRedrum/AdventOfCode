44226057ca6b4314dcb76bd5c4f9ed82be072ab4
Ryan Sweeney <swee.ryan@gmail.com>
2018-12-01 12:42:38 -0500

    Updated tests, and made actual solution only run when command line argument is present.

--- 2018/Day1/Part1.js
+++ 2018/Day1/Part1.js
@@ -6,8 +6,14 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 const calculateFrequency = data => data.reduce(reducer);
 exports.calculateFrequency = calculateFrequency;
 
-const inputPath = path.relative(process.cwd(), './Part1Input.txt');
-fileReader.readIntsFromFile(inputPath, data => {
-    const total = calculateFrequency(data);
-    console.log(total);
-});
+const findSolution = () => {
+    const inputPath = path.relative(process.cwd(), './Part1Input.txt');
+    fileReader.readIntsFromFile(inputPath, data => {
+        const total = calculateFrequency(data);
+        console.log(total);
+    });
+};
+
+if (process.argv[2] && process.argv[2] === 'execute') {
+    findSolution();
+}
